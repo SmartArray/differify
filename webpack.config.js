6b8cb5e500e4d84ae6c6aa5dfe121d51b435187f
Yoshi Jaeger <github@jaeger.berlin>
2024-09-24 17:44:58 +0200

    chore: added commonjs bundling to webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports = {
+const umdConfig = {
   mode: 'production',
   entry: './src/differify.ts',
   output: {
@@ -27,3 +27,27 @@ module.exports = {
   }
 };
 
+const commonjsConfig = {
+    mode: 'production',
+    entry: './src/differify.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist', 'commonjs'),
+        filename: 'differify.commonjs.js',
+        libraryTarget: 'commonjs2',
+        libraryExport: 'default',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+};
+
+module.exports = [commonjsConfig, umdConfig];
