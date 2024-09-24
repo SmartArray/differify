209bb248a43908738648b28732c907aecea98ed5
Yoshi Jaeger <github@jaeger.berlin>
2024-09-24 16:30:53 +0200

    chore: created new build pipeline that creates umd and node packages separately

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,29 @@
 const path = require('path');
-const TypescriptDeclarationPlugin = require('typescript-declaration-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'differify.ts'),
-  output: {
-    path: path.join(__dirname),
-    filename: 'index.js',
-    library: 'Differify',
-    libraryExport: 'default',
-    libraryTarget: 'umd',
-    globalObject: 'this',
-    umdNamedDefine: true,
-  },
   mode: 'production',
+  entry: './src/differify.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist', 'umd'),
+    filename: 'differify.umd.js',
+    libraryTarget: 'umd',
+    library: 'Differify',
+    umdNamedDefine: true,
+    globalObject: 'this'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
   module: {
     rules: [
       {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-        },
-      },
-      {
         test: /\.ts$/,
-        loader: 'ts-loader',
-        include: [/src/],
-      },
-      {
-        test: /\.(j|t)s$/,
-        loader: 'webpack-comment-remover-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
-  plugins: [
-    new TypescriptDeclarationPlugin({
-      out: 'index.d.ts',
-    }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
+  externals: {
+  }
 };
+
